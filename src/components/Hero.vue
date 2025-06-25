d371d4a481821a02f489d246f7a33e5536490820
miebale123 <miebalen@gmail.com>
2025-06-25 21:03:40 +0300

    fixed_for_maintainability

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import fireworkAtom from './Molecules/fireworkAtom.vue'
 import programmer from '../assets/programmer.json'
 import { Vue3Lottie } from 'vue3-lottie'
-// import flameAtom from './Atoms/flameAtom.vue'
 </script>
 
 <template>
   <main>
-    <fireworkAtom class="mt-22" />
-    <div class="bg-black flex items-center justify-center">
-      <div class="text-6xl bg-gradient-to-r via-yellow-100">
+    <div class="bg-gray-900 mt-20  justify-center">
+      <div class="text-6xl bg-gradient-to-r via-yellow-300 absolute m-20">
             congratulations!
       </div>
       <br />
